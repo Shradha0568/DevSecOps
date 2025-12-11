b269c21baa1c148a29d62d1fb212bed90b002877
ASR91103 <shradha91103@gmail.com>
2025-12-11 12:42:38 +0530

    Initial Code

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve everything from the public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Netflix UI Clone running on port ${PORT}`);
+});
